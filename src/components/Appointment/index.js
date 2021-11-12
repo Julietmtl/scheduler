232ebb9f14c9bec7d5233bcb77d90edf52d1b7db
Juliet <li_juliet@hotmail.com>
2021-11-11 20:57:33 -0500

    add netlify file

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,56 +1,61 @@
-import React, { Fragment } from 'react'
-import './styles.scss'
-import Header from './Header.js'
-import Show from './Show.js'
-import Empty from './Empty.js'
-import Form from './Form.js'
-import Status from './Status.js'
-import Confirm from './Confirm.js'
-import Error from './Error.js'
-import useVisualMode from 'hooks/useVisualMode'
+import React, { Fragment } from "react";
+import "./styles.scss";
+import Header from "./Header.js";
+import Show from "./Show.js";
+import Empty from "./Empty.js";
+import Form from "./Form.js";
+import Status from "./Status.js";
+import Confirm from "./Confirm.js";
+import Error from "./Error.js";
+import useVisualMode from "hooks/useVisualMode";
+import axios from "axios";
 
-export default function Appointment (props) {
-  const EMPTY = 'EMPTY'
-  const SHOW = 'SHOW'
-  const CREATE = 'CREATE'
-  const SAVING = 'SAVING'
-  const CONFIRM = 'CONFIRM'
-  const DELETING = 'DELETING'
-  const EDIT = 'EDIT'
-  const ERROR_SAVE = 'ERROR_SAVE'
-  const ERROR_DELETE = 'ERROR_DELETE'
+export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const SAVING = "SAVING";
+  const CONFIRM = "CONFIRM";
+  const DELETING = "DELETING";
+  const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
+
+  if (process.env.REACT_APP_API_BASE_URL) {
+    axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+  }
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
-  )
+  );
 
-  function save (name, interviewer) {
+  function save(name, interviewer) {
     const interview = {
       student: name,
-      interviewer
-    }
-    transition(SAVING)
+      interviewer,
+    };
+    transition(SAVING);
     props
       .bookInterview(props.id, interview)
       .then(() => transition(SHOW))
-      .catch(err => transition(ERROR_SAVE, true))
+      .catch((err) => transition(ERROR_SAVE, true));
   }
 
-  function confirmDelete () {
-    transition(DELETING, true)
+  function confirmDelete() {
+    transition(DELETING, true);
     props
       .cancelInterview(props.id)
       .then(() => transition(EMPTY))
-      .catch(err => transition(ERROR_DELETE, true))
+      .catch((err) => transition(ERROR_DELETE, true));
   }
 
   function edit() {
-    transition(EDIT)
+    transition(EDIT);
   }
 
   return (
     <Fragment>
-      <article className='appointment'>
+      <article className="appointment">
         <Header time={props.time} />
         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
         {mode === SHOW && (
@@ -70,13 +75,13 @@ export default function Appointment (props) {
             onSave={save}
           />
         )}
-        {mode === SAVING && <Status message={'Saving'} />}
-        {mode === DELETING && <Status message={'Deleting'} />}
+        {mode === SAVING && <Status message={"Saving"} />}
+        {mode === DELETING && <Status message={"Deleting"} />}
         {mode === CONFIRM && (
           <Confirm
             onConfirm={confirmDelete}
             onCancel={() => back()}
-            message={'Are you sure you want to delete?'}
+            message={"Are you sure you want to delete?"}
           />
         )}
         {mode === EDIT && (
@@ -90,17 +95,17 @@ export default function Appointment (props) {
         )}
         {mode === ERROR_SAVE && (
           <Error
-            message={'Could not save appointment'}
+            message={"Could not save appointment"}
             onClose={() => back()}
           />
         )}
         {mode === ERROR_DELETE && (
           <Error
-            message={'Could not cancel appointment'}
+            message={"Could not cancel appointment"}
             onClose={() => back()}
           />
         )}
       </article>
     </Fragment>
-  )
+  );
 }
